a2f3387941add6ce841c32d263d19386f7e3bbfa
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2020-08-14 10:13:54 -0400

    add web structure

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Landing from 'pages/Landing';
+
+import 'styles/global.css';
+
+const App: React.FC = () => {
+  return <Landing />;
+};
+
+export default App;
---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Landing: React.FC = () => {
+  // const sound = new Audio(content.audioContent);
+
+  // const start = (): void => {
+  //   sound.play();
+  // };
+
+  return (
+    <>
+      <h1>How to Pronounce</h1>
+      <button onClick={() => console.log('click')}>Ouvir</button>
+    </>
+  );
+};
+
+export default Landing;
